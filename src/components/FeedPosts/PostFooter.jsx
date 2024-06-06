0ac1c638d12023de39ecd195f8dbac3eddc4093f
arifmanawer <112596030+arifmanawer@users.noreply.github.com>
2024-06-06 16:57:49 -0400

    finished profile page

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -2,7 +2,7 @@ import { Box, Button, Flex, Input, InputGroup, InputRightElement, Text } from "@
 import { useState } from "react";
 import { CommentLogo, NotificationsLogo, UnlikeLogo } from "../../assets/constants";
 
-const PostFooter = ({ username }) => {
+const PostFooter = ({ username, isProfilePage }) => {
     const [liked, setLiked] = useState(false);
     const [likes, setLikes] = useState(1000);
     const handleLike = () => {
@@ -17,7 +17,7 @@ const PostFooter = ({ username }) => {
 
     return (
     <>
-    <Box mb={10}>
+    <Box mb={10} marginTop={"auto"}>
     <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4} >
         <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
             {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
@@ -29,7 +29,9 @@ const PostFooter = ({ username }) => {
     <Text fontWeight={600} fontSize={"sm"}>
         {likes} likes
     </Text>
-    <Text fontSize={"sm"} fontWeight={700}>
+    {!isProfilePage && (
+        <>
+        <Text fontSize={"sm"} fontWeight={700}>
         {username} {" "}
         <Text as={"span"} fontWeight={400}>
             wowowwoowow
@@ -38,6 +40,10 @@ const PostFooter = ({ username }) => {
     <Text fontSize={"sm"} color={"gray"}>
         View all 1,000 comments
     </Text>
+    </>
+    )}
+
+
     <Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"}>
         <InputGroup>
             <Input variant={"flushed"} placeholder={"Add a comment ..."} fontSize={14}/>
